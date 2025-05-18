0f42375628bb305c392cebbde4170e9c2235ba8b
Adam <adamziv156@gmail.com>
2025-05-18 14:20:16 -0500

    :added another model

---
+++ Migrations/20250518191934_AddedUserAddressModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TBD.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedUserAddressModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Models/UserAddress.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TBD.Models;
+
+[Table("Address")]
+public class UserAddress : GenericEntity
+{
+    [Required] public Guid UserId { get; set; }
+
+
+    [ForeignKey(nameof(UserId))] public User User { get; set; }
+
+    [Required] public string? Address1 { get; set; }
+    public string? Address2 { get; set; }
+    [Required] public string? City { get; set; }
+    [Required] public string? State { get; set; }
+    [Required] public int? ZipCode { get; set; }
+}
