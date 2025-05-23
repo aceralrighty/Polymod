8544cb0fb30b2b0aab39f0670d6f23f861685383
Adam <adamziv156@gmail.com>
2025-05-23 11:41:48 -0500

    :Changed the controller

---
+++ API/DTOs/UserAddressResponse.cs
@@ -0,0 +1,13 @@
+namespace TBD.API.DTOs
+{
+    public class UserAddressResponse
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string? Address1 { get; set; }
+        public string? Address2 { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public int ZipCode { get; set; }
+    }
+}
--- Shared/Utils/UserAddressMapping.cs
+++ Shared/Utils/UserAddressMapping.cs
@@ -11,5 +11,6 @@ public class UserAddressMapping : Profile
         CreateMap<UserAddressRequest, UserAddress>()
             .ForMember(dest => dest.UserId, opt => opt.Condition(src => src.UserId != Guid.Empty))
             .ForAllMembers(opt => opt.Condition((src, dest, srcMember) => srcMember != null));
+        CreateMap<UserAddress, UserAddressResponse>();
     }
 }
