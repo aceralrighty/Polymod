72b4e1a083109037f3fbdd3936383a56c72fb1df
Adam Ziv <adamziv156@gmail.com>
2025-06-01 17:07:20 -0500

    :corrected my calculation to include times under an hour

--- ServiceModule/Data/ServiceDbContext.cs
+++ ServiceModule/Data/ServiceDbContext.cs
@@ -18,6 +18,7 @@ public class ServiceDbContext(DbContextOptions<ServiceDbContext> options) : DbCo
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Service>().HasIndex(u => u.Id).IsUnique();
-        modelBuilder.Entity<Service>().Property(u => u.TotalPrice).HasComputedColumnSql("Price * DurationInMinutes");
+        modelBuilder.Entity<Service>().Property(u => u.TotalPrice).HasComputedColumnSql(
+            "CASE \nWHEN [DurationInMinutes] < 60 \nTHEN ([Price] / 60.0 * [DurationInMinutes]) \nELSE [Price] * ([DurationInMinutes] / 60.0) \nEND");
     }
 }
