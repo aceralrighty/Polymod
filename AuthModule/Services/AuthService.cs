707132974fa8fdb58615fc2e54478824104d8480
Adam Ziv <adamziv156@gmail.com>
2025-06-06 11:15:56 -0500

    :nothing major

--- AuthModule/Services/AuthService.cs
+++ AuthModule/Services/AuthService.cs
@@ -14,7 +14,7 @@ internal class AuthService : IAuthService, IAuthRepository
     private readonly DbSet<AuthUser> _dbSet;
     private readonly IConfiguration _configuration;
     private readonly ILogger<AuthService> _logger;
-    private readonly IHasher Hasher;
+    private readonly IHasher _hasher;
 
     public AuthService(AuthDbContext dbContext, IConfiguration configuration, ILogger<AuthService> logger,
         IHasher hasher)
@@ -23,14 +23,14 @@ internal class AuthService : IAuthService, IAuthRepository
         _dbSet = _dbContext.Set<AuthUser>();
         _configuration = configuration;
         _logger = logger;
-        Hasher = hasher;
+        _hasher = hasher;
     }
 
     public async Task<AuthUser?> AuthenticateAsync(string username, string password)
     {
         var authUser = await GetAuthUserByUsernameAsync(username);
         if (authUser == null) return null;
-        if (Hasher.Verify(authUser.HashedPassword, password))
+        if (_hasher.Verify(authUser.HashedPassword, password))
         {
             authUser.LastLogin = DateTime.UtcNow;
             authUser.FailedLoginAttempts = 0;
@@ -77,7 +77,7 @@ internal class AuthService : IAuthService, IAuthRepository
                 Id = Guid.NewGuid(),
                 Username = request.Username,
                 Email = request.Email,
-                HashedPassword = Hasher.HashPassword(request.Password),
+                HashedPassword = _hasher.HashPassword(request.Password),
                 FailedLoginAttempts = 0,
             };
             await _dbSet.AddAsync(createNewUser);
