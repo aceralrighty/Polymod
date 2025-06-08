07cc688f38ac167b791d02e3750f205779a52306
Adam Ziv <adamziv156@gmail.com>
2025-06-08 12:07:53 -0500

    :formmatted crap

--- UserModule/Models/User.cs
+++ UserModule/Models/User.cs
@@ -1,6 +1,5 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using TBD.AuthModule.Models;
 using TBD.GenericDBProperties;
 using TBD.ScheduleModule.Models;
 
