22550873f498cda9e0f88ae45442e571299b02ce
Adam Ziv <adamziv156@gmail.com>
2025-06-08 13:03:52 -0500

    :Had to fix some stuff

--- AddressModule/Models/UserAddress.cs
+++ AddressModule/Models/UserAddress.cs
@@ -14,13 +14,14 @@ public class UserAddress
     [ForeignKey(nameof(UserId))]
     public User User { get; init; }
 
-    [Required][MaxLength(255)] public string Address1 { get; set; }
+    [Required] [MaxLength(255)] public string Address1 { get; set; }
     [MaxLength(255)] public string? Address2 { get; set; }
-    [Required][MaxLength(255)] public string? City { get; set; }
-    [Required][MaxLength(255)] public string? State { get; set; }
+    [Required] [MaxLength(255)] public string? City { get; set; }
+    [Required] [MaxLength(255)] public string? State { get; set; }
 
     [Required]
     [RegularExpression(@"^[0-9]{5}(?:-[0-9]{4})?$", ErrorMessage = "Invalid ZIP code format. Use 12345 or 12345-6789.")]
+    [MaxLength(10)]
     public string ZipCode { get; set; }
 
     public UserAddress(Guid userId, User user, string? address1, string? address2, string? city, string? state,
