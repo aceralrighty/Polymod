a2580c40c0155faf97c054850ca0d398388f4e85
Adam Ziv <adamziv156@gmail.com>
2025-06-09 13:00:02 -0500

    :Got rid of the singleton

--- Program.cs
+++ Program.cs
@@ -17,7 +17,6 @@ builder.Services.AddAddressService(builder.Configuration);
 builder.Services.AddScheduleModule(builder.Configuration);
 builder.Services.AddServiceModule(builder.Configuration);
 builder.Services.AddAuthModule(builder.Configuration);
-builder.Services.AddSingleton<JwtTokenGenerator>();
 builder.Services.AddAuthorization();
 builder.Services.AddControllersWithViews();
 builder.Services.AddOpenApi();
