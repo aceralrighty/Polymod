b0c1b9160431b9792781b3360b751e7c9fffa802
Adam Ziv <adamziv156@gmail.com>
2025-06-10 13:33:07 -0500

    :Got rid of method contracts that i didn't need

--- AuthModule/Repositories/IAuthRepository.cs
+++ AuthModule/Repositories/IAuthRepository.cs
@@ -11,14 +11,10 @@ public interface IAuthRepository
     Task<AuthUser?> GetUserByEmail(string email);
 
     Task<IEnumerable<AuthUser>> GetUsersByLoginAttempts(int loginAttempts);
-    Task<IEnumerable<AuthUser>> GetAllAsync();
-    Task<IEnumerable<AuthUser>> FindAsync(Expression<Func<AuthUser, bool>> predicate);
 
     Task<AuthUser?> GetUserByRefreshToken(string refreshToken);
     Task InvalidateRefreshTokenAsync(Guid userId);
 
     Task AddAsync(AuthUser entity);
     Task UpdateAsync(AuthUser entity);
-    Task DeleteAsync(AuthUser entity);
 }
-
