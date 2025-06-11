e5dfec3abf59b0f2c81519d4bc52d53821453c62
Adam Ziv <adamziv156@gmail.com>
2025-06-11 14:53:07 -0500

    :adding context

--- RecommendationModule/Data/RecommendationDbContext.cs
+++ RecommendationModule/Data/RecommendationDbContext.cs
@@ -1,6 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using TBD.RecommendationModule.Models;
+
 namespace TBD.RecommendationModule.Data;
 
-public class RecommendationDbContext
+public class RecommendationDbContext : DbContext
 {
-    
+    public DbSet<Recommendation> Recommendations { get; set; }
+
+    public RecommendationDbContext(DbContextOptions<RecommendationDbContext> options) : base(options) { }
+    public RecommendationDbContext() { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseInMemoryDatabase("FallbackTestDb");
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Recommendation>(entity =>
+        {
+            entity.HasKey(r => r.Id);
+            entity.HasOne(r => r.User)
+                .WithMany()
+                .HasForeignKey(r => r.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+            entity.HasOne(r => r.Service).WithMany().HasForeignKey(r => r.ServiceId).OnDelete(DeleteBehavior.Cascade);
+        });
+    }
 }
